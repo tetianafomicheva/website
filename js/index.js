aa2c847bd35dfaf5756c8ac342e437dadd4e0c76
Tetiana <tetyana.fomscheva@mail.ru>
2016-12-17 06:32:36 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// JavaScript File
+ btn.addEventListener ('click', funcion (e) {
+    var str = "#Comment";
+    var res = str.split('').join ('');
+    result.innerHTML=res;
+    e.preventDefault();
+    
+ });
